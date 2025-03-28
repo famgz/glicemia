a4f4ffe31f00fb88cc985c1d2447948d0f977760
famgz <famgz@proton.me>
2025-03-28 20:33:07 -0300

    fix: input control warning

--- src/components/buttons/login.tsx
+++ src/components/buttons/login.tsx
@@ -11,7 +11,7 @@ interface Props {
 export default async function LoginButton({ redirect }: Props) {
   return (
     <form action={login} className="flex items-center justify-between">
-      <input type="hidden" name="redirect" value={redirect} />
+      <input type="hidden" name="redirect" value={redirect || ''} />
       <input type="hidden" name="login-provider" value="google" />
       <Button
         className="flex w-full items-center gap-2"
