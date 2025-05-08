e86d41bce18cd0600173793cd579986185268886
Famgz <famgz@proton.me>
2025-05-08 17:01:17 -0300

    feat: make user page's separate display components as client to avoid duplicate requests.

---
+++ src/app/user/[slug]/components/content-wrapper.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { GlucoseLog } from '@prisma/client';
+import {
+  ChartColumnDecreasingIcon,
+  Grid3x3Icon,
+  LucideIcon,
+  TableOfContentsIcon,
+} from 'lucide-react';
+import { useState } from 'react';
+
+import GlucoseLogsCharts from '@/components/glucose-log/charts';
+import GlucoseLogList from '@/components/glucose-log/list';
+import GlucoseLogTable from '@/components/glucose-log/table';
+import { Button } from '@/components/ui/button';
+
+type DisplayType = 'list' | 'table' | 'chart';
+
+const displayTypesMap: Record<
+  DisplayType,
+  { label: string; Icon: LucideIcon }
+> = {
+  table: { label: 'Tabela', Icon: Grid3x3Icon },
+  list: { label: 'Lista', Icon: TableOfContentsIcon },
+  chart: { label: 'Gráfico', Icon: ChartColumnDecreasingIcon },
+};
+
+const DEFAULT_DISPLAY_MODE = 'table';
+
+interface Props {
+  glucoseLogs: GlucoseLog[];
+}
+
+export default function ContentWrapper({ glucoseLogs }: Props) {
+  const [mode, setMode] = useState(DEFAULT_DISPLAY_MODE);
+
+  return (
+    <>
+      <div className="flex items-center justify-end gap-2 print:hidden">
+        {Object.entries(displayTypesMap).map(([key, { label, Icon }]) => (
+          <Button
+            key={key}
+            variant={mode === key ? 'default' : 'outline'}
+            className="flex items-center gap-2"
+            onClick={() => setMode(key)}
+          >
+            <span className="max-[250px]:hidden">{label}</span>
+            <Icon />
+          </Button>
+        ))}
+      </div>
+      <div>
+        {mode === 'table' && <GlucoseLogTable glucoseLogs={glucoseLogs} />}
+        {mode === 'list' && <GlucoseLogList glucoseLogs={glucoseLogs} />}
+        {mode === 'chart' && <GlucoseLogsCharts glucoseLogs={glucoseLogs} />}
+      </div>
+    </>
+  );
+}
--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,44 +1,21 @@
 'use server';
 
-import {
-  ChartColumnDecreasingIcon,
-  Grid3x3Icon,
-  LucideIcon,
-  TableOfContentsIcon,
-} from 'lucide-react';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import { getUserBySlugWithGlucoseLogs } from '@/actions/user';
+import ContentWrapper from '@/app/user/[slug]/components/content-wrapper';
 import { auth } from '@/auth';
 import { SocialShareButton } from '@/components/buttons/social-share';
-import GlucoseLogsCharts from '@/components/glucose-log/charts';
-import GlucoseLogList from '@/components/glucose-log/list';
-import GlucoseLogTable from '@/components/glucose-log/table';
 import { Button } from '@/components/ui/button';
-
-type DisplayType = 'list' | 'table' | 'chart';
-
-const displayTypesMap: Record<
-  DisplayType,
-  { label: string; Icon: LucideIcon }
-> = {
-  table: { label: 'Tabela', Icon: Grid3x3Icon },
-  list: { label: 'Lista', Icon: TableOfContentsIcon },
-  chart: { label: 'Gráfico', Icon: ChartColumnDecreasingIcon },
-};
-
-const DEFAULT_DISPLAY_TYPE = 'table';
 
 interface Props {
   params: Promise<{ slug: string }>;
-  searchParams: Promise<{ mode: DisplayType }>;
 }
 
-export default async function UserPage({ params, searchParams }: Props) {
+export default async function UserPage({ params }: Props) {
   const loggedInUser = (await auth())?.user;
   const slug = (await params).slug;
-  const mode = (await searchParams).mode || DEFAULT_DISPLAY_TYPE;
   const dbUser = await getUserBySlugWithGlucoseLogs(slug);
   const glucoseLogs = dbUser?.glucoseLogs;
 
@@ -59,29 +36,9 @@ export default async function UserPage({ params, searchParams }: Props) {
           <SocialShareButton url={`https://glicemia.vercel.app/user/${slug}`} />
         </div>
       </div>
-      <div className="flex items-center justify-end gap-2 print:hidden">
-        {Object.entries(displayTypesMap).map(([key, { label, Icon }]) => (
-          <Button
-            asChild
-            variant={mode === key ? 'default' : 'outline'}
-            key={key}
-          >
-            <Link
-              href={`/user/${slug}?mode=${key}`}
-              className="flex items-center gap-2"
-            >
-              <span className="max-[250px]:hidden">{label}</span>
-              <Icon />
-            </Link>
-          </Button>
-        ))}
-      </div>
+
       {glucoseLogs?.length > 0 ? (
-        <div>
-          {mode === 'list' && <GlucoseLogList glucoseLogs={glucoseLogs} />}
-          {mode === 'table' && <GlucoseLogTable glucoseLogs={glucoseLogs} />}
-          {mode === 'chart' && <GlucoseLogsCharts glucoseLogs={glucoseLogs} />}
-        </div>
+        <ContentWrapper glucoseLogs={glucoseLogs} />
       ) : (
         <div className="text-muted-foreground flex flex-col items-center gap-10 py-20 text-center">
           <span>Nenhuma registro encontrado.</span>
